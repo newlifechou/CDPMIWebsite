dedad92cf96bd4a0b67bc11ca43e154288ee87a7
newlifechou <newlifechou@gmail.com>
2014-10-22 09:26:49 +0800

    自上次git出问题之后，所有的change

---
+++ MvcApplication_CDPMI/Controllers/ProductController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication_CDPMI.Models;
+
+namespace MvcApplication_CDPMI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private WebEntities db = new WebEntities();
+
+        //
+        // GET: /Product/
+
+        public ActionResult Index()
+        {
+            var product = db.product.Include(p => p.productCategory);
+            return View(product.ToList());
+        }
+
+        //
+        // GET: /Product/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            product product = db.product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        //
+        // GET: /Product/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.categoryID = new SelectList(db.productCategory, "categoryID", "categoryName");
+            return View();
+        }
+
+        //
+        // POST: /Product/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.product.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.categoryID = new SelectList(db.productCategory, "categoryID", "categoryName", product.categoryID);
+            return View(product);
+        }
+
+        //
+        // GET: /Product/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            product product = db.product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.categoryID = new SelectList(db.productCategory, "categoryID", "categoryName", product.categoryID);
+            return View(product);
+        }
+
+        //
+        // POST: /Product/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.categoryID = new SelectList(db.productCategory, "categoryID", "categoryName", product.categoryID);
+            return View(product);
+        }
+
+        //
+        // GET: /Product/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            product product = db.product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        //
+        // POST: /Product/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            product product = db.product.Find(id);
+            db.product.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
