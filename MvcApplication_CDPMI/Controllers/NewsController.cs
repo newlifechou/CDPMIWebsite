d39bbf6abfbde10b9c1b5913e085f6af2da34c72
newlifechou <newlifechou@gmail.com>
2014-10-28 15:55:58 +0800

    添加新闻编辑的控制器及其视图

---
+++ MvcApplication_CDPMI/Controllers/NewsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication_CDPMI.Models;
+
+namespace MvcApplication_CDPMI.Controllers
+{
+    public class NewsController : Controller
+    {
+        private WebEntities db = new WebEntities();
+
+        //
+        // GET: /News/
+
+        public ActionResult Index()
+        {
+            return View(db.news.ToList());
+        }
+
+        //
+        // GET: /News/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            news news = db.news.Find(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+
+        //
+        // GET: /News/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /News/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(news news)
+        {
+            if (ModelState.IsValid)
+            {
+                db.news.Add(news);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(news);
+        }
+
+        //
+        // GET: /News/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            news news = db.news.Find(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+
+        //
+        // POST: /News/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(news news)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(news).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(news);
+        }
+
+        //
+        // GET: /News/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            news news = db.news.Find(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+
+        //
+        // POST: /News/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            news news = db.news.Find(id);
+            db.news.Remove(news);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
