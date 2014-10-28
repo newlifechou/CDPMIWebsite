546bac6dcfe9376d1134e5939c52bd3d5b7d6965
newlifechou <newlifechou@gmail.com>
2014-10-28 15:06:19 +0800

    添加产品类别编辑的控制器及其视图

---
+++ MvcApplication_CDPMI/Controllers/ProductCategoryController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication_CDPMI.Models;
+
+namespace MvcApplication_CDPMI.Controllers
+{
+    public class ProductCategoryController : Controller
+    {
+        private WebEntities db = new WebEntities();
+
+        //
+        // GET: /ProductCategory/
+
+        public ActionResult Index()
+        {
+            return View(db.productCategory.ToList());
+        }
+
+        //
+        // GET: /ProductCategory/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            productCategory productcategory = db.productCategory.Find(id);
+            if (productcategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productcategory);
+        }
+
+        //
+        // GET: /ProductCategory/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /ProductCategory/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(productCategory productcategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.productCategory.Add(productcategory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(productcategory);
+        }
+
+        //
+        // GET: /ProductCategory/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            productCategory productcategory = db.productCategory.Find(id);
+            if (productcategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productcategory);
+        }
+
+        //
+        // POST: /ProductCategory/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(productCategory productcategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productcategory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(productcategory);
+        }
+
+        //
+        // GET: /ProductCategory/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            productCategory productcategory = db.productCategory.Find(id);
+            if (productcategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productcategory);
+        }
+
+        //
+        // POST: /ProductCategory/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            productCategory productcategory = db.productCategory.Find(id);
+            db.productCategory.Remove(productcategory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
