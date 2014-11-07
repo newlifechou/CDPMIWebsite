fb5b42a77a4ab1bd841189f9e009ab2f87b672aa
newlifechou <newlifechou@gmail.com>
2014-11-07 21:20:57 +0800

    增加了动态切换slide的功能，更改了全部slide图片

--- MvcApplication_CDPMI/Scripts/js/MainSlider.js
+++ MvcApplication_CDPMI/Scripts/js/MainSlider.js
@@ -1,9 +1,26 @@
 ﻿$(function () {
     var index = 0;
+    //设置自动切换动画效果
+    var adTimer;
+    var len = $(".num>li").length;
+
     $(".num li").mouseover(function () {
         index = $(".num li").index(this);
         showImg(index);
     }).eq(0).mouseover();
+
+    //滑入停止动画，滑出开始动画
+    $("#ad").hover(function () {
+        clearInterval(adTimer);
+    }, function () {
+        adTimer = setInterval(function () {
+            showImg(index);
+            index++;
+            if (index == len) {
+                index = 0;
+            }
+        }, 3000);
+    }).trigger("mouseleave");
 
     function showImg(index) {
         var adHeight = $("#ad").height();
@@ -12,4 +29,6 @@
         $(".num li").removeClass("selected")
             .eq(index).addClass("selected");
     }
+
+
 });
