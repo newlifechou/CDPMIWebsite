c7ab78494c047aa728b292b3821243f38b0a804f
newlifechou <newlifechou@gmail.com>
2014-11-20 09:02:04 +0800

    some note

--- MvcApplication_PMIEnglishSite/Controllers/HomeController.cs
+++ MvcApplication_PMIEnglishSite/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+//添加Models命名空间
+using MvcApplication_PMIEnglishSite.Models;
 
 namespace MvcApplication_PMIEnglishSite.Controllers
 {
     public class HomeController : Controller
     {
+
         //
         // GET: /Home/
 
