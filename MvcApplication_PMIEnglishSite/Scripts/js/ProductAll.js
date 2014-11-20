e3640e8419946b1b34d7dd74deab9fe457434862
newlifechou <newlifechou@gmail.com>
2014-11-20 16:27:07 +0800

    图片更改

---
+++ MvcApplication_PMIEnglishSite/Scripts/js/ProductAll.js
@@ -0,0 +1,7 @@
+﻿$(function () {
+    $("#productlist ul li").mouseover(function () {
+        $(this).addClass("plistitem");
+    }).mouseout(function () {
+        $(this).removeClass("plistitem");
+    });
+});
