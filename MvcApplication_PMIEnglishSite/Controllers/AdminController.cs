dca5c4f226d0ef707287841b5b7a02a294435142
newlifechou <newlifechou@gmail.com>
2014-11-21 16:18:17 +0800

    contact 添加在主导航栏上

---
+++ MvcApplication_PMIEnglishSite/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication_PMIEnglishSite.Controllers
+{
+    public class AdminController : Controller
+    {
+        //Admin welcome Page
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
