184d735cf39652eb7ddcc7672aeea11bd367ef1c
newlifechou <newlifechou@gmail.com>
2014-11-22 20:06:48 +0800

    对Models的备份 cdpmi pmi

---
+++ Model bk/Models-pmi/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication_PMIEnglishSite.Models
+{
+    /// <summary>
+    /// 用户验证类
+    /// </summary>
+    public class UserRepository
+    {
+        pmienglish db = new pmienglish();
+        public bool ValidateUser(string userName,string passWord)
+        {
+            return db.admin_en.Any(u => u.userName == userName && u.passWord == passWord);
+        }
+    }
+}
