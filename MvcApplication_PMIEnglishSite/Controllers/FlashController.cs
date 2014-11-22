6c45b5a732e82559c88bae7452217cf4f89a397e
newlifechou <newlifechou@gmail.com>
2014-11-22 08:49:46 +0800

    添加Service 和 Flash

---
+++ MvcApplication_PMIEnglishSite/Controllers/FlashController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication_PMIEnglishSite.Models;
+
+namespace MvcApplication_PMIEnglishSite.Controllers
+{
+    public class FlashController : Controller
+    {
+        private pmienglish db = new pmienglish();
+
+        //
+        // GET: /Flash/
+
+        public ActionResult Index()
+        {
+            return View(db.flash_en.ToList());
+        }
+
+        //
+        // GET: /Flash/Details/5
+
+        //public ActionResult Details(int id = 0)
+        //{
+        //    flash_en flash_en = db.flash_en.Find(id);
+        //    if (flash_en == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(flash_en);
+        //}
+
+        //
+        // GET: /Flash/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Flash/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(flash_en flash_en)
+        {
+            if (ModelState.IsValid)
+            {
+                db.flash_en.Add(flash_en);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(flash_en);
+        }
+
+        //
+        // GET: /Flash/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            flash_en flash_en = db.flash_en.Find(id);
+            if (flash_en == null)
+            {
+                return HttpNotFound();
+            }
+            return View(flash_en);
+        }
+
+        //
+        // POST: /Flash/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(flash_en flash_en)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(flash_en).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(flash_en);
+        }
+
+        //
+        // GET: /Flash/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            flash_en flash_en = db.flash_en.Find(id);
+            if (flash_en == null)
+            {
+                return HttpNotFound();
+            }
+            return View(flash_en);
+        }
+
+        //
+        // POST: /Flash/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            flash_en flash_en = db.flash_en.Find(id);
+            db.flash_en.Remove(flash_en);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcApplication_PMIEnglishSite/Controllers/ServiceController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication_PMIEnglishSite.Models;
+
+namespace MvcApplication_PMIEnglishSite.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private pmienglish db = new pmienglish();
+
+        //
+        // GET: /Service/
+
+        public ActionResult Index()
+        {
+            return View(db.service_en.ToList());
+        }
+
+        //
+        // GET: /Service/Details/5
+
+        //public ActionResult Details(int id = 0)
+        //{
+        //    service_en service_en = db.service_en.Find(id);
+        //    if (service_en == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(service_en);
+        //}
+
+        //
+        // GET: /Service/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Service/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(service_en service_en)
+        {
+            if (ModelState.IsValid)
+            {
+                db.service_en.Add(service_en);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(service_en);
+        }
+
+        //
+        // GET: /Service/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            service_en service_en = db.service_en.Find(id);
+            if (service_en == null)
+            {
+                return HttpNotFound();
+            }
+            return View(service_en);
+        }
+
+        //
+        // POST: /Service/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(service_en service_en)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(service_en).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(service_en);
+        }
+
+        //
+        // GET: /Service/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            service_en service_en = db.service_en.Find(id);
+            if (service_en == null)
+            {
+                return HttpNotFound();
+            }
+            return View(service_en);
+        }
+
+        //
+        // POST: /Service/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            service_en service_en = db.service_en.Find(id);
+            db.service_en.Remove(service_en);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
