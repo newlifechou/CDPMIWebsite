27acbbd0922e01626b5643a30f8d9b850a736c74
newlifechou <newlifechou@gmail.com>
2014-11-28 09:05:56 +0800

    fix the model bug

--- MvcApplication_PMIEnglishSite/Models/feedback_en.cs
+++ MvcApplication_PMIEnglishSite/Models/feedback_en.cs
@@ -6,6 +6,7 @@
 //    Manual changes to this file will be overwritten if the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcApplication_PMIEnglishSite.Models
 {
@@ -15,11 +16,26 @@ namespace MvcApplication_PMIEnglishSite.Models
     public partial class feedback_en
     {
         public int id { get; set; }
+        [Display(Name = "Title")]
+        [Required(ErrorMessage = "It cannot be null")]
+        [StringLength(50, ErrorMessage = "The length should be shorter than 50 words")]
         public string title { get; set; }
+        [Display(Name = "Create Time")]
         public System.DateTime createTime { get; set; }
+        [Display(Name = "MainContent")]
+        [Required(ErrorMessage = "It cannot be null")]
         public string mainContent { get; set; }
+        [Display(Name = "Phone")]
+        [StringLength(50, ErrorMessage = "The length should be shorter than 50 words")]
         public string phone { get; set; }
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "It cannot be null")]
+        [StringLength(50, ErrorMessage = "The length should be shorter than 50 words")]
+        [RegularExpression(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage = "It is not a email address")]
         public string email { get; set; }
+        [Display(Name = "Customer Name")]
+        [Required(ErrorMessage = "It cannot be null")]
+        [StringLength(50, ErrorMessage = "The length should be shorter than 50 words")]
         public string cname { get; set; }
     }
 }
